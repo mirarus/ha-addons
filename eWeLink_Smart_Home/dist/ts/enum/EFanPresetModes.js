dc5c0090c1d030538d502f95d9aa925d62355bda
Ali Güçlü <aliguclutr@gmail.com>
2026-04-23 20:09:47 +0300

    Remove eWeLink_Smart_Home add-on
    Delete the entire eWeLink_Smart_Home add-on directory, including source, build/dist artifacts, bundled node_modules, docs, config, translations, and related assets. This cleans up the repository by removing the legacy eWeLink Smart Home add-on and its generated files.

--- eWeLink_Smart_Home/dist/ts/enum/EFanPresetModes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EFanPresetMode;
-(function (EFanPresetMode) {
-    EFanPresetMode["Low"] = "Low";
-    EFanPresetMode["Medium"] = "Medium";
-    EFanPresetMode["High"] = "High";
-})(EFanPresetMode || (EFanPresetMode = {}));
-exports.default = EFanPresetMode;
